fdea85f01cfd0f266800f9f46b2eabdf6198d51b
radioxid <zmindster@gmail.com>
2011-05-15 16:23:52 +0200

    Added Exceptions. Misconception among split_in_scopes: returns GlobalScopes' content & isn't ready yet to output multiple scopes

--- langh.py
+++ langh.py
@@ -1,13 +1,45 @@
 ﻿#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-# 
+# Put your code into 'input'
+#   I'll try not to use RegEx. It'll be hard!
 
-# input = open('./code.txt').read().decode('utf-8')
-input = ''' ( (pattern) ê (replacement) ) '''
+input = '''( ((pattern) ê (replacement)) (ê) )'''
 
 # ① Obliger GlobalScope
 # ② Obliger PatternScheme
+
+
+class LanghException(Exception):
+  """
+  Langh's most basic Exception.
+  """
+  def __init__( self,  w ):
+    self.reason = w
+    print(self)
+  def __str__( self ):
+    return self.__class__.__name__ + ': ' + self.reason
+
+class LanghError(LanghException):
+  """
+  Langh's Error. It stops compilation.
+  """
+  def __init__( self,  w ):
+    LanghException.__init__(self, w)
+    from sys import exit
+    exit(1)
+
+class LanghWarning(LanghException):
+  """
+  Langh's Warning. Compilation continues.
+  """
+  pass
+
+class SyntaxError(LanghError):
+  pass
+class GrammarError(LanghError):
+  pass
+
 
 
 def split_in_scopes( input ):
@@ -29,34 +61,47 @@ def split_in_scopes( input ):
     nn = n
     n = new
 
-  for i in range(len(c)):
-    if c[i] == '(':
-      #change_n(n + 1)
-      nn = n  # n(-1)
-      n = n+1 # n(0)
-      if nn != 0:
-        buffer += c[i]
-    elif c[i] == ')':
-      #change_n(n - 1)
-      nn = n  # n(-1)
-      n = n-1 # n(0)
-      if n == 0:
-        #print(buffer)#
-        scopes.append(buffer)
-        buffer = ''
-      buffer += c[i]
-    else:
-      buffer += c[i]
-    #print(n)#
+  try:
+    for i in range(len(c)):
+      if c[i] == '(':
+        #change_n(n + 1)
+        nn = n  # n(-1)
+        n = n+1 # n(0)
+        if nn != 0:
+          buffer += c[i]
 
-  if n < 0:  # Une ')' trouvée, sans paire '(':
-    # Le dernier scope est celui en cause
-    print("SyntaxError: " \
-      "cannot find (Global||local)Scope's ending" '\n' \
-      "In: " + scopes[-1] + ')' '\n' \
-      "Near " + '-'*(len(scopes[-1]) +4 -5 -1) + '↗' '\n' \
-      '\t' "(unmatching left parenthesis)"
-    )
+      elif c[i] == ')':
+        #change_n(n - 1)
+        nn = n  # n(-1)
+        n = n-1 # n(0)
+        if n == 0:
+          #print(buffer)#
+          scopes.append(buffer)
+          buffer = ''
+        buffer += c[i]
+
+      else:
+        buffer += c[i]
+
+      #print(n)#
+
+
+    if len(scopes) == 0:
+      raise GrammarError(
+        "you didn't mind to embed your code in the GlobalScope, didn't you?")
+
+    if n < 0:  # Une ')' trouvée, sans paire '(':
+      # Le dernier scope est celui en cause
+      raise SyntaxError(
+        "cannot find (Global||local)Scope's ending" '\n' \
+        "In: " + scopes[len(scopes) -1] + ')' '\n' \
+        "Near " + '-'*(len("In: ") + len(scopes[len(scopes) -1]) -len("Near ") 
+          -len(')')) + '↗' '\n' \
+        '\t' "(unmatching left parenthesis)"
+      )
+
+  except LanghException as e:
+    pass
 
   print(scopes)#
   return scopes
@@ -67,6 +112,7 @@ scopes = split_in_scopes(input)
 n = nn = 0
 # ② Appliquer les schemes
 for scope in scopes:
+  split_in_scopes(scope)
   c = scope
   for i in range(len(c)):
     pass
