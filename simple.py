5d75abb469959d525753401503022061e1c4b7b3
Pierre Fenoll <pierrefenoll@gmail.com>
2012-03-30 20:11:19 +0200

    rsync ⌘S

--- simple.py
+++ simple.py
@@ -7,25 +7,26 @@ import sys
 raw = open(sys.argv[1]).read().decode('utf-8')
 
 import re
-lateral = 'jilk'; middle = '-|-'
-scheme = re.compile(lateral +" \s*(.+)\s* \-\|\-(?: \s*(\-i)\s* \-\|\-)? \s*(.+)\s* "+ lateral)
+lateral = 'jilk'; middle = '-|-' #add escape_regex/1
+scheme = re.compile(lateral +"\s*(.+)\s*\-\|\-(?:\s*(.+)\s*\-\|\-)?\s*(.+)\s*"+ lateral)
 
-def apply( raw ):
-  match = scheme.search(raw)
-  if not match:
+def apply( raw ): #may be function of scheme someday
+  global scheme
+  match = scheme.search(raw)  # Greedy or not greedy?
+  if not match:  # Cannot apply any longer
     return raw
 
   (pattern, modifier, replacement) = match.groups()
-  #raw = re.sub(regex, '', raw, 1)
   pos_start = match.start(); pos_end = match.end()
   raw = raw[:pos_start] +raw[pos_end:]
 
-  flags = 0
+  flags = 0 #more flags to come
   if modifier == u'-i':
     flags = re.IGNORECASE
   regex = re.compile(pattern, flags)
   raw = re.sub(regex, replacement, raw)
 
-  return raw
+  print raw, "Applied: ", pattern, " --> ", replacement, " / ", modifier
+  return apply(raw)  # Keeps applying
 
 print apply(raw)
